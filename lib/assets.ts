9203f806f231b1015a6376a8599c5a4629f4bd7f
furkantiv <eiusbey@gmail.com>
2025-11-27 11:59:35 +0300

    Update default basePath for asset resolution in development environment

--- lib/assets.ts
+++ lib/assets.ts
@@ -1,4 +1,4 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "/iaflmun";
 
 export function asset(path: string) {
   if (!path.startsWith("/")) {
